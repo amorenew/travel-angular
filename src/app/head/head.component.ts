d35228d6dde794f135b1c406ac463990bb5e2c46
amr <amr@mailcohen.com>
2018-01-24 10:48:23 +0300

    bootstrap 3 and scss

--- src/app/head/head.component.ts
+++ src/app/head/head.component.ts
@@ -3,7 +3,7 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-head',
   templateUrl: './head.component.html',
-  styleUrls: ['./head.component.sass']
+  styleUrls: ['./head.component.scss']
 })
 export class HeadComponent implements OnInit {
 
