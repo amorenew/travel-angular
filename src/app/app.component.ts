0682094e859948168c5df1f093c285f16ff5a1fe
amr <amr@mailcohen.com>
2018-01-24 18:14:31 +0300

    add title bar

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,22 +1,30 @@
-import { Component } from '@angular/core';
-import { FrameworkConfigService, FrameworkConfigSettings } from '../framework/services/framework-config.service';
+import {Component} from '@angular/core';
+import {FrameworkConfigService, FrameworkConfigSettings} from '../framework/services/framework-config.service';
 
-@Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
-})
+@Component({selector: 'app-root', templateUrl: './app.component.html', styleUrls: ['./app.component.scss']})
 export class AppComponent {
-constructor(private frameworkConfigService:FrameworkConfigService){
-  let config:FrameworkConfigSettings={
-    socialIcons:[{imageFile:'assets/ic_social_fb',alt:'Facebook',link:'https:/www.facebook.com'},
-    {imageFile:'assets/ic_social_tw',alt:'Twitter',link:'https:/www.twitter.com'},
-    {imageFile:'assets/ic_social_gp',alt:'Google +',link:'https:/www.Google.com'}],
-    showLanguageSelector:true,
-    showUserControls:true,
-    showStatusBar:true,
-    showStatusBarBreakpoint:800
-  };
-  frameworkConfigService.configure(config);
-}
+  constructor(private frameworkConfigService : FrameworkConfigService) {
+    let config : FrameworkConfigSettings = {
+      socialIcons: [
+        {
+          imageFile: 'assets/ic_social_fb.svg',
+          alt: 'Facebook',
+          link: 'https:/www.facebook.com'
+        }, {
+          imageFile: 'assets/sample.png',
+          alt: 'Twitter',
+          link: 'https:/www.twitter.com'
+        }, {
+          imageFile: 'assets/ic_logo_header.png',
+          alt: 'Google +',
+          link: 'https:/www.Google.com'
+        }
+      ],
+      showLanguageSelector: true,
+      showUserControls: true,
+      showStatusBar: true,
+      showStatusBarBreakpoint: 800,
+    };
+    frameworkConfigService.configure(config);
+  }
 }
--- src/framework/top-bar/top-bar.component.ts
+++ src/framework/top-bar/top-bar.component.ts
@@ -1,14 +1,17 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit,ViewEncapsulation } from '@angular/core';
 import { FrameworkConfigService } from '../services/framework-config.service';
 
 @Component({
   selector: 'fw-top-bar',
   templateUrl: './top-bar.component.html',
-  styleUrls: ['./top-bar.component.scss']
+  styleUrls: ['./top-bar.component.scss'],
+  encapsulation:ViewEncapsulation.None
 })
 export class TopBarComponent implements OnInit {
 
-  constructor(private frameworkConfigService:FrameworkConfigService) { }
+  constructor(private frameworkConfigService:FrameworkConfigService) { 
+    console.log(frameworkConfigService);
+  }
 
   ngOnInit() {
   }
