6f4ee2577b3f94b6b3d47354618730eed954b56f
amr <amr@mailcohen.com>
2018-01-29 17:24:37 +0300

    toobar finish and before adding menus

---
+++ src/framework/directives/screen-large.directive.ts
@@ -0,0 +1,46 @@
+import {Directive, Input, TemplateRef, ViewContainerRef} from "@angular/core";
+
+import {ScreenService} from "../services/screen.service";
+import {Subscription} from "rxjs/Subscription";
+import {OnDestroy} from "@angular/core/src/metadata/lifecycle_hooks";
+
+@Directive({selector: '[screenLarge]'})
+export class ScreenLarge implements OnDestroy {
+
+    private hasView = false;
+    private screenSubscription : Subscription;
+
+    constructor(private viewContainer : ViewContainerRef, private template : TemplateRef < Object >, private screenService : ScreenService) {
+
+        this.screenSubscription = screenService
+            .resize$
+            .subscribe(() => this.onResize());
+    }
+
+    @Input()
+    set screenLarge(condition) {
+        // ignore the passed condition and set it based on screen size
+        condition = this.screenService.screenWidth >= this.screenService.largeBreakpoint;
+
+        if (condition && !this.hasView) {
+            this.hasView = true;
+            this
+                .viewContainer
+                .createEmbeddedView(this.template);
+        } else if (!condition && this.hasView) {
+            this.hasView = false;
+            this
+                .viewContainer
+                .clear();
+        }
+    }
+    ngOnDestroy() {
+        this
+            .screenSubscription
+            .unsubscribe();
+    }
+    onResize() {
+        // trigger the setter
+        this.screenLarge = false;
+    }
+}
--- src/framework/framework.module.ts
+++ src/framework/framework.module.ts
@@ -1,21 +1,27 @@
-import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
-import { FrameworkBodyComponent } from './framework-body/framework-body.component';
-import { ContentComponent } from './content/content.component';
-import { TitleBarComponent } from './title-bar/title-bar.component';
-import { FrameworkConfigService } from './services/framework-config.service';
-import { TopBarComponent } from './top-bar/top-bar.component';
-import { StatusBarComponent } from './status-bar/status-bar.component';
-import { ScreenService } from './services/screen.service';
+import {NgModule} from '@angular/core';
+import {CommonModule} from '@angular/common';
+import {FrameworkBodyComponent} from './framework-body/framework-body.component';
+import {ContentComponent} from './content/content.component';
+import {TitleBarComponent} from './title-bar/title-bar.component';
+import {FrameworkConfigService} from './services/framework-config.service';
+import {TopBarComponent} from './top-bar/top-bar.component';
+import {StatusBarComponent} from './status-bar/status-bar.component';
+import {ScreenService} from './services/screen.service';
+import {ScreenLarge} from './directives/screen-large.directive';
+import {ScreenBelowLarge} from './directives/screen-below-large.directive';
 
 @NgModule({
-  imports: [
-    CommonModule
-  ],
+  imports: [CommonModule],
   declarations: [
-    FrameworkBodyComponent,ContentComponent,TitleBarComponent,TopBarComponent,StatusBarComponent
+    FrameworkBodyComponent,
+    ContentComponent,
+    TitleBarComponent,
+    TopBarComponent,
+    StatusBarComponent,
+    ScreenLarge,
+    ScreenBelowLarge
   ],
-  exports:[FrameworkBodyComponent],
-  providers:[FrameworkConfigService,ScreenService]
+  exports: [FrameworkBodyComponent],
+  providers: [FrameworkConfigService, ScreenService]
 })
-export class FrameworkModule { }
+export class FrameworkModule {}
