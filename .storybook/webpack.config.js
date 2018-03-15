30ac647144c47aee2c3fa6ce48882d6522c7e09f
AmrEniou <amr@mailcohen.com>
2018-03-15 15:00:46 +0300

    add material icon

--- .storybook/webpack.config.js
+++ .storybook/webpack.config.js
@@ -1,18 +1,37 @@
 const path = require('path');
-
-module.exports = {
-    module: {
-        rules: [{
-            test: /\.md$/,
-            use: [
-              {
-                loader: 'html-loader',
-              },
-              {
-                loader: 'markdown-loader',
-              },
-            ],
-            include: path.resolve(__dirname, '../')
-          }]
-    }
+module.exports = (baseConfig, env) => {
+  baseConfig.resolve.alias = {
+    ...baseConfig.resolve.alias,
+    'sass': path.join(process.cwd(), 'src/sass'),
+  };
+  return baseConfig;
 }
+
+
+// const path = require('path');
+
+// module.exports = {
+//     module: {
+//         rules: [{
+//             test: /\.md$/,
+//             use: [
+//               {
+//                 loader: 'html-loader',
+//               },
+//               {
+//                 loader: 'markdown-loader',
+//               },
+//             ],
+//             include: path.resolve(__dirname, '../')
+//           }],{
+//              baseConfig.resolve.alias = {
+//         ...baseConfig.resolve.alias,
+//         'sass': path.join(process.cwd(), 'src/sass'),
+//     };
+//           }
+//     }
+
+
+
+
+// }
